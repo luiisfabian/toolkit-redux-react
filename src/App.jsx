794ba63e4fc659e5849d5a12aa094fa84fbce70c
FabianM <luiisfabian96@gmail.com>
2024-02-22 10:52:09 -0500

    implementacion del decremente y incrementBy

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { useDispatch, useSelector } from 'react-redux'
-import { increment } from './store/slices/counter';
+import { decrement, increment, incrementByAmount } from './store/slices/counter';
 
 function App() {
 
-  const { counter } =  useSelector(state => state.counter);
-  const dispatch =  useDispatch();
+  const { counter } = useSelector(state => state.counter);
+  const dispatch = useDispatch();
 
   return (
     <>
@@ -19,16 +19,29 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
+      <p>          count is {counter}
+      </p>
       <div className="card">
         <button onClick={() => (
           dispatch(increment())
         )}>
-          count is {counter}
+          Increment
         </button>
-    
+        <button onClick={() => (
+          dispatch(incrementByAmount(2))
+        )}>
+          Increment by 2
+        </button>
+        <button onClick={() => (
+          dispatch(decrement())
+        )}>
+          Decrement
+        </button>
+
       </div>
-  
+
     </>
   )
 }
